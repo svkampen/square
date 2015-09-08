902d25d899cd0734b67728d2b2b19c5b27103e34
Sam van Kampen <sam@tehsvk.net>
2015-09-08 21:26:59 +0200

    Add color conversion functions (RGB -> CIELab)

---
+++ colorconvert.py
@@ -0,0 +1,33 @@
+def rgb2xyz(arr):
+    arr = [it/255 for it in arr]
+    arr = [((it + 0.055) / 1.055) ** 2.4 if it > 0.04045 else it / 12.92 for it in arr]
+    arr = [it * 100 for it in arr]
+
+    X = arr[0] * 0.4124 + arr[1] * 0.3576 + arr[2] * 0.1805
+    Y = arr[0] * 0.2126 + arr[1] * 0.7152 + arr[2] * 0.0722
+    Z = arr[0] * 0.0193 + arr[1] * 0.1192 + arr[2] * 0.9505
+    return [X,Y,Z]
+
+def xyz2lab(arr):
+    ref_x = 95.047
+    ref_y = 100.000
+    ref_z = 108.883
+
+    arr[0] /= ref_x
+    arr[1] /= ref_y
+    arr[2] /= ref_z
+
+    for n, item in enumerate(arr):
+        if ( item > 0.008856 ):
+            arr[n] **= (1/3)
+        else:
+            arr[n] = (7.787 * arr[n]) + (16 / 116)
+
+    CIE_L = (116 * arr[1]) - 16
+    CIE_a = 500 * (arr[0] - arr[1])
+    CIE_b = 200 * (arr[1] - arr[2])
+
+    return (CIE_L, CIE_a, CIE_b)
+
+def rgb2lab(arr):
+    return xyz2lab(rgb2xyz(arr))
