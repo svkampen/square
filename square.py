dc0e939b72fe48a616329d67433dd576970de86e
Sam van Kampen <sam@tehsvk.net>
2015-09-13 21:11:42 +0200

    Add square function (divide an area into squares)

--- square.py
+++ square.py
@@ -29,3 +29,29 @@ def remove_array(L,arr):
         L.pop(ind)
     else:
         raise ValueError('array not found in list.')
+
+def sq(data):
+    """Split an image into squares. If the image's greatest common divisor is
+    <64, just quarter it. If x == y, also quarter. Returns them in some order,
+    can't remember which (figure it out yourself)"""
+    data_size   = Vector2(*data.shape[:2])
+    edge_length = gcd(data_size.x, data_size.y)
+
+    if (data_size.x == data_size.y):
+        return quarter(data)
+
+    if (edge_length < 64):
+        return quarter(data)
+
+    n_squares = Vector2(data_size.x / edge_length, data_size.y / edge_length)
+
+    slices = []
+
+    for i in range(int(n_squares.y)):
+        for j in range(int(n_squares.x)):
+            print("Slicing from [%d:%d] (x) and [%d:%d] (y)" % (i*edge_length,
+                i*edge_length + edge_length, j * edge_length, j*edge_length+edge_length))
+            slices.append(get_slice(data, Area(i * edge_length, j * edge_length,
+                edge_length, edge_length)))
+
+    return slices
