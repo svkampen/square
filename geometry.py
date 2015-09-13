4813e5dab3e3f1e4ae8084ab80487291cf06cc62
Sam van Kampen <sam@tehsvk.net>
2015-09-13 20:07:53 +0200

    Add Area definition to geometry.py

--- geometry.py
+++ geometry.py
@@ -1,5 +1,9 @@
 class Area():
-    pass
+    def __init__(self, x, y, dx, dy):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
 
 class Vector2():
     """ A vector with an x-coordinate and a y-coordinate. """
